46ad7ba1a5d176acb852ae3ffe9f49fda7f97b2d
beurkinger <macnuggets@gmail.com>
2018-06-07 17:36:11 +0200

    created state + default state + state altering methods

--- src/App.js
+++ src/App.js
@@ -2,15 +2,74 @@ import React, { Component } from 'react';
 import AudioPlayer from './components/audio/AudioPlayer';
 
 class App extends Component {
+  state = {
+    players: [
+      {
+        id: 0,
+        manipulationItensity: 0,
+        manipulationType: 'lowpass',
+        isPlaying: false,
+        wavFile: 'bass.wav',
+      },
+      {
+        id: 1,
+        manipulationItensity: 0,
+        manipulationType: 'lowpass',
+        isPlaying: false,
+        wavFile: 'bells.wav',
+      },
+      {
+        id: 2,
+        manipulationItensity: 0,
+        manipulationType: 'lowpass',
+        isPlaying: false,
+        wavFile: 'highhats.wav',
+      },
+      {
+        id: 3,
+        manipulationItensity: 0,
+        manipulationType: 'lowpass',
+        isPlaying: false,
+        wavFile: 'strings.wav',
+      },
+      {
+        id: 4,
+        manipulationItensity: 0,
+        manipulationType: 'lowpass',
+        isPlaying: false,
+        wavFile: 'woodblock.wav',
+      }
+    ]
+  }
+
+  setPlayerIntensity = (playerId, intensity = 0) => {
+    const players = this.state.players.map(player => {
+      return (player.id === playerId)
+        ? { ...player, intensity }
+        : player;
+    });
+    this.setState({ players });
+  }
+
+  switchPlayer = (playerId, isPlaying = false) => {
+    const players = this.state.players.map(player => {
+      return (player.id === playerId)
+        ? { ...player, isPlaying }
+        : player;
+    });
+    this.setState({ players });
+  }
+
+  // FOR TESTING PURPOSES
+  // componentDidMount () {
+  //   this.setPlayerIntensity(2, 0.5);
+  //   this.switchPlayer(2, true);
+  // }
 
   render() {
     return (
       <div>
-				<AudioPlayer wavFile="bass.wav" manipulationValue={this.state.arrayOfLayers[0].value} isPlaying={true} manipulationType="lowpass" />
-				<AudioPlayer wavFile="bells.wav" manipulationValue={this.state.arrayOfLayers[1].value} isPlaying={true} manipulationType="lowpass" />
-				<AudioPlayer wavFile="highhats.wav" manipulationValue={this.state.arrayOfLayers[2].value} isPlaying={true} manipulationType="lowpass" />
-				<AudioPlayer wavFile="strings.wav" manipulationValue={this.state.arrayOfLayers[3].value} isPlaying={true} manipulationType="lowpass" />
-				<AudioPlayer wavFile="woodblock.wav" manipulationValue={this.state.arrayOfLayers[4].value} isPlaying={true} manipulationType="lowpass" />
+        { this.state.players.map(player => <AudioPlayer key={ player.id } { ...player } />) }
       </div>
     );
   }
