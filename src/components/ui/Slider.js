a4213e76fb944d685fb09bea65c13deeb3aa0084
beurkinger <macnuggets@gmail.com>
2018-06-07 19:18:41 +0200

    auto back stepping in slider

--- src/components/ui/Slider.js
+++ src/components/ui/Slider.js
@@ -2,6 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './Slider.css';
 
+const MIN_VALUE = 0;
+const MAX_VALUE = 1;
+const STEP_VALUE = 0.02;
+const DELAY = 1000;
+const TIME_FROM_MAX_TO_MIN = 5000;
+const INTERVAL = TIME_FROM_MAX_TO_MIN * STEP_VALUE;
+
 class Slider extends React.Component {
   static propTypes = {
     id: PropTypes.number.isRequired,
@@ -10,9 +17,40 @@ class Slider extends React.Component {
   };
 
   numberFormat = new Intl.NumberFormat('en-EN', { minimumFractionDigits: 2 });
+  interval = null;
+  timeout = null;
+
+  componentWillUnmount () {
+    if (this.timeout) clearTimeout(this.timeout);
+    if (this.interval) clearInterval(this.interval);
+  }
 
   handleChange = e => {
     this.props.onUpdate(this.props.id, parseFloat(e.target.value));
+    this.startIntervalWithDelay();
+  }
+
+  startIntervalWithDelay = () => {
+    if (this.timeout) clearTimeout(this.timeout);
+    if (this.interval) clearInterval(this.interval);
+
+    this.timeout = setTimeout(() => {
+      this.startInterval();
+    }, DELAY);
+  }
+
+  startInterval = () => {
+    this.interval = setInterval(() => {
+      const { manipulationIntensity } = this.props;
+
+      const newManipulationIntensity = (manipulationIntensity - STEP_VALUE >= 0)
+        ? manipulationIntensity - STEP_VALUE
+        : 0;
+
+      this.props.onUpdate(this.props.id, newManipulationIntensity);
+      
+      if (newManipulationIntensity <= 0) clearInterval(this.interval);
+    }, INTERVAL)
   }
 
   render() {
@@ -23,10 +61,10 @@ class Slider extends React.Component {
         </span>
         <input 
           className="slider__input"
-          max="1" 
-          min="0" 
+          max={ MAX_VALUE } 
+          min={ MIN_VALUE } 
           onChange={ this.handleChange }
-          step="0.05"
+          step={ STEP_VALUE }
           type="range" 
           value={ this.props.manipulationIntensity } 
         />
